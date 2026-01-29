fa257d86287b0a418c186fd730a4483221ad38e0
Keerthi <keerthi.renduchintala@ucsf.edu>
2026-01-28 21:42:01 -0800

    wrote function for construct_mst

--- mst/graph.py
+++ mst/graph.py
@@ -42,3 +42,46 @@ class Graph:
 
         """
         self.mst = None
+        # ensure matrix is constructed properly
+        i = self.adj_mat.shape[0]
+        j = self.adj_mat.shape[1]
+        if i != j:
+            raise ValueError 
+        else:
+            nodes = i
+            # initialize output MST as zeros
+            self.mst = np.zeros((nodes,nodes))
+            # track visited nodes
+            visited = set()
+            #choose start node at random
+            start_node = np.random.randint(0,nodes)
+            # add start node to visited
+            visited.add(start_node)
+            # make heap que for tracking and sorting edge weight
+            heap_priority_queue = []
+            # initialize heap_priority_list with (weight, start, neighbor) for start_node's possible neighbors
+            for neighbor in range(nodes):
+                weight = self.adj_mat[start_node][neighbor]
+                if weight > 0:
+                    heapq.heappush(heap_priority_queue,(weight, start_node, neighbor))
+
+            # while heap priority list queue is not empty, find the top node from heap list and add it to visited, then go to visited,
+            # and add it to visited, then go to visited and add neighbors if neighbors not in visited and weight > 0 to heap que. 
+            # adjust self.mst (undirected!)
+            while len(heap_priority_queue) > 0:
+                weight, from_node, to_node = heapq.heappop(heap_priority_queue)
+                if to_node not in visited:
+                    visited.add(to_node)
+                    self.mst[from_node][to_node] = weight
+                    self.mst[to_node][from_node] = weight
+                    for neighbor in range(nodes):
+                        weight = self.adj_mat[to_node][neighbor]
+                        if neighbor not in visited and weight > 0:
+                            heapq.heappush(heap_priority_queue,(weight,to_node, neighbor))
+
+            
+
+
+
+
+        
